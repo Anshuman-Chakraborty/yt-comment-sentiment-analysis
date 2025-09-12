c10e139cc838a60046e53e785b02103e7037610b
Anshuman-Chakraborty <anshumanc.in@gmail.com>
2025-09-12 22:33:04 +0530

    add promote model script

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -1,14 +1,32 @@
+import os
 import mlflow
-from mlflow.tracking import MlflowClient
 
-mlflow.set_tracking_uri("http://ec2-13-200-250-45.ap-south-1.compute.amazonaws.com:5000/")
-client = MlflowClient()
+def promote_model():
+    # Set up AWS MLflow tracking URI
+    mlflow.set_tracking_uri("http://ec2-13-200-250-45.ap-south-1.compute.amazonaws.com:5000/")
 
-# Change version number if needed
-client.transition_model_version_stage(
-    name="yt_chrome_plugin_model",
-    version=6,
-    stage="Staging"
-)
+    client = mlflow.MlflowClient()
 
-print("✅ yt_chrome_plugin_model version 6 promoted to Staging")
+    model_name = "yt_chrome_plugin_model"
+    # Get the latest version in staging
+    latest_version_staging = client.get_latest_versions(model_name, stages=["Staging"])[0].version
+
+    # Archive the current production model
+    prod_versions = client.get_latest_versions(model_name, stages=["Production"])
+    for version in prod_versions:
+        client.transition_model_version_stage(
+            name=model_name,
+            version=version.version,
+            stage="Archived"
+        )
+
+    # Promote the new model to production
+    client.transition_model_version_stage(
+        name=model_name,
+        version=latest_version_staging,
+        stage="Production"
+    )
+    print(f"Model version {latest_version_staging} promoted to Production")
+
+if __name__ == "__main__":
+    promote_model()
